29ec12dc18f46b6c718037758decd5df58cf3608
Daddy-Jardi <jardielgaming@gmail.com>
2023-09-26 18:27:36 -0400

    completed playRound function

--- script.js
+++ script.js
@@ -9,7 +9,39 @@ function getComputerChoice() {
     } else {
         return 'scissors';
     }
-    
 }
 
-console.log(getComputerChoice());
+function playRound(playerSelection, computerSelection){
+   
+    if(playerSelection.toLowerCase() == 'rock' && computerSelection == 'rock'){
+        return "It's a draw! You both selected Rock!"
+    }
+    else if(playerSelection.toLowerCase() == 'paper' && computerSelection == 'paper'){
+        return "It's a draw! You both selected Paper!"
+    } 
+    else if(playerSelection.toLowerCase() == 'scissors' && computerSelection == 'scissors'){
+        return "It's a draw! You both selected Scissors!"
+    }
+    else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'paper'){
+        return "You Lose! Paper beats Rock!"
+    }
+    else if (playerSelection.toLowerCase() == 'rock' && computerSelection == 'scissors'){
+        return "You Win! Rock beats Scissors!"
+    }
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'rock'){
+        return "You Win! Paper beats Rock!"
+    }
+    else if (playerSelection.toLowerCase() == 'paper' && computerSelection == 'scissors'){
+        return "You Lose! Scissors beats Paper!"
+    }
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'paper'){
+        return "You Win! Scissors beats Paper!"
+    }
+    else if (playerSelection.toLowerCase() == 'scissors' && computerSelection == 'rock'){
+        return "You Lose! Rock beats Scissors!"
+    }
+        
+}
+const playerSelection = prompt('Paper, Rock or Scissors!');
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
